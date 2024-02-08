af2c3a9a5ebb22302248617a8b2ed1fda6c61a40
lmoyap <luciamoyap2@gmail.com>
2024-02-08 19:37:19 +0100

    suma

--- operaciones/index.js
+++ operaciones/index.js
@@ -1,26 +1,29 @@
 /**
  * Aquesta funcio suma tots els valors passats per parametre
- * 
+ *
  * @param  {...Number} valors els valors amb els que realitzar la operacio
  * @returns {Number} el resultat de la operacio
  */
 function suma(...valors) {
-
+  let resultat = 0;
+  for (let num of nums) {
+    resultat += num;
+  }
+  return resultat;
 }
-
 
 /**
  * Aquesta funcio multiplica tots els valors passats per parametre
- * 
+ *
  * @param  {...Number} valors els valors amb els que realitzar la operacio
  * @returns {Number} el resultat de la operacio
  */
 function multiplica(...valors) {
-    let total = valors[0];
-    for (let i = 1; i < valors.length; i++) {
-        total *= valors[i];
-    }
-    return total;
+  let total = valors[0];
+  for (let i = 1; i < valors.length; i++) {
+    total *= valors[i];
+  }
+  return total;
 }
 
 module.exports.suma = suma;
