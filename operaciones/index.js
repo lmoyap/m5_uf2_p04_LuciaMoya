0a007f09c4ffcaafe47136b2f99c1e5a340b2864
MariaFD00 <mariafuentes.alum@insestatut.cat>
2024-02-08 19:30:22 +0100

    multi

--- operaciones/index.js
+++ operaciones/index.js
@@ -16,7 +16,11 @@ function suma(...valors) {
  * @returns {Number} el resultat de la operacio
  */
 function multiplica(...valors) {
-
+    let total = valors[0];
+    for (let i = 1; i < valors.length; i++) {
+        total *= valors[i];
+    }
+    return total;
 }
 
 module.exports.suma = suma;
